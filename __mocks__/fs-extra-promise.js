99867c61ece47974aab2dc310b43423e294a1e1f
nicholaschuayunzhi <nicholaschuayunzhi@gmail.com>
2018-02-21 16:01:16 +0800

    Using memfs as in memory file system
    Have to rewrite all node-extra and -promise, methods. Need to read
    documentation carefully.

--- __mocks__/fs-extra-promise.js
+++ __mocks__/fs-extra-promise.js
@@ -1,57 +1,165 @@
 // Mock specific functions of fs-extra-promise module
-const fs = jest.genMockFromModule('fs-extra-promise');
 const path = require('path');
+const { fs, vol } = require('memfs');
 
-fs.mockFileSystem;
+// fs.mockFileSystem;
 
-/**
- * Mocking fs#readFileSync:
- * https://nodejs.org/api/fs.html#fs_fs_readfilesync_path_options
- * @param filepath
- * @param options
- * @returns {string}
- */
-
-fs.readFileSync = (filepath, options) => fs.mockFileSystem.getFileContent(filepath);
-
-
-/**
- * Mocking fs-extra#emptydirSync
- *
- * @param filePath
- */
-fs.emptydirSync = (filePath) => {
-};
+// /**
+//  * Mocking fs#readFileSync:
+//  * https://nodejs.org/api/fs.html#fs_fs_readfilesync_path_options
+//  * @param filepath
+//  * @param options
+//  * @returns {string}
+//  */
+//
+//
+// fs.readFileSync = fs.readFileSync;
+//
+//
+// /**
+//  * Mocking fs-extra#emptydirSync
+//  *
+//  * @param filePath
+//  */
+// fs.emptydirSync = (filePath) => {
+// };
 
 /**
  * Mocking fs-extra-promise#readJsonAsync
- *
- * @param filePath
+ * TODO: Error throwing
  */
-fs.readJsonAsync = filePath => Promise.resolve(fs.mockFileSystem.getFileContent(filePath));
+fs.readJsonAsync = filePath => Promise.resolve(JSON.parse(fs.readFileSync(filePath, 'utf8')));
+
+/**
+ * Remove directory recursively
+ * @param {string} dirPath
+ * @see https://stackoverflow.com/a/42505874/3027390
+ */
+function rimraf(dirPath) {
+  if (fs.existsSync(dirPath)) {
+    fs.readdirSync(dirPath).forEach((entry) => {
+      const entryPath = path.join(dirPath, entry);
+      if (fs.lstatSync(entryPath).isDirectory()) {
+        rimraf(entryPath);
+      } else {
+        fs.unlinkSync(entryPath);
+      }
+    });
+    fs.rmdirSync(dirPath);
+  }
+}
+
+/**
+ * fs#copyFileSync not implemented in memfs
+ * this does not accurately mock fs#copyFileSync behaviour as it should not accept directories
+ * @see https://stackoverflow.com/a/26038979
+ */
+function copyFileSync(src, dest) {
+  let targetFile = dest;
+
+  // if target is a directory a new file with the same name will be created
+  if (fs.existsSync(dest)) {
+    if (fs.lstatSync(dest).isDirectory()) {
+      targetFile = path.join(dest, path.basename(src));
+    }
+  }
+  console.log(src);
+  console.log(targetFile);
+  console.log(fs.readFileSync(src))
+  console.log(src);
+  fs.writeFileSync(targetFile, fs.readFileSync(src));
+}
+
+/**
+ * @see https://stackoverflow.com/a/26038979
+ */
+function copyDirRecursiveSync(src, dest) {
+  let files = [];
+  // check if folder needs to be created or integrated
+  const targetFolder = path.join(dest, path.basename(src));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  // copy
+  if (fs.lstatSync(src).isDirectory()) {
+    files = fs.readdirSync(src);
+    files.forEach((file) => {
+      const curSource = path.join(src, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyDirRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
+}
+/**
+ * Mocking fs-extra#emptydirSync
+ */
+fs.emptydirSync = (dir) => {
+  rimraf(dir);
+  return fs.mkdirSync(dir);
+};
+
+/**
+ * Mocking fs-extra-promise#removeAsync
+ */
+
+fs.removeAsync = pathArg => new Promise((resolve, reject) => {
+  try {
+    if (fs.lstatSync(pathArg).isDirectory()) {
+      rimraf(pathArg);
+    } else {
+      fs.unlinkSync(pathArg);
+    }
+    resolve();
+  } catch (err) {
+    reject(err);
+  }
+});
 
 /**
  * Mocking fs-extra-promise#copyAsync
- *
- * @param filePath
  */
-fs.copyAsync = (src, dest, options) => new Promise((resolve, reject) => {
-  const pathObj = path.parse(src);
-  const isDirectory = pathObj.ext === '';
 
-  if (isDirectory) {
-    const newDirectory = fs.mockFileSystem.createDirectory(dest);
-    const directory = fs.mockFileSystem.getDirectory(src);
-    Object.keys(directory.files)
-      .forEach(fileName => newDirectory.storeFileInDir(fileName, directory.files[fileName].content));
-
-    Object.keys(directory.subDirectories).forEach((subDirName) => {
-      fs.copyAsync(path.join(src, subDirName), path.join(dest, subDirName));
-    });
-  } else {
-    fs.mockFileSystem.storeFileInDir(dest);
+fs.copyAsync = (src, dest) => new Promise((resolve, reject) => {
+  console.log(src, dest);
+  try {
+    if (fs.lstatSync(src).isDirectory()) {
+      copyDirRecursiveSync(src, dest);
+    } else {
+      copyFileSync(src, dest);
+    }
+  } catch (err) {
+    reject(err);
   }
-  resolve();
 });
+// /**
+//  * Mocking fs-extra-promise#copyAsync
+//  *
+//  * @param filePath
+//  */
+// fs.copyAsync = (src, dest, options) => new Promise((resolve, reject) => {
+//   const pathObj = path.parse(src);
+//   const isDirectory = pathObj.ext === '';
+//
+//   if (isDirectory) {
+//     const newDirectory = fs.mockFileSystem.createDirectory(dest);
+//     const directory = fs.mockFileSystem.getDirectory(src);
+//     Object.keys(directory.files)
+//       .forEach(fileName => newDirectory.storeFileInDir(fileName, directory.files[fileName].content));
+//
+//     Object.keys(directory.subDirectories).forEach((subDirName) => {
+//       fs.copyAsync(path.join(src, subDirName), path.join(dest, subDirName));
+//     });
+//   } else {
+//     fs.mockFileSystem.storeFileInDir(dest);
+//   }
+//   resolve();
+// });
+//
+// fs.mkdirSync = fs.mkdirSync;
 
+fs.vol = vol;
 module.exports = fs;
--- __tests__/Site.test.js
+++ __tests__/Site.test.js
@@ -69,39 +69,87 @@ mockFileSystem.storeFile(getPath('asset', 'js', 'vue-strap.min.js'));
 
 
 test('Site Generate builds the correct amount of assets', () => {
-  fs.mockFileSystem = mockFileSystem;
-  walkSync.mockFileSystem = mockFileSystem;
-  const site = new Site(path.resolve(''), path.resolve('_site'));
-  return site.generate().then(() => {
-    // _site folder
-    expect(mockFileSystem.containsDirectory(getPath('_site'))).toBeTruthy();
+  const json = {
+    './lib/template/page.ejs':
+      '<!DOCTYPE html>\n'
+      + '<html>\n'
+      + '<head>\n'
+      + '    <meta charset="utf-8">\n'
+      + '    <meta http-equiv="X-UA-Compatible" content="IE=edge">\n'
+      + '    <meta name="viewport" content="width=device-width, initial-scale=1">\n'
+      + '    <title><%= title %></title>\n'
+      + '    <link rel="stylesheet" href="<%- asset.bootstrap %>">\n'
+      + '    <link rel="stylesheet" href="<%- asset.highlight %>">\n'
+      + '    <link rel="stylesheet" href="<%- asset.markbind %>">\n'
+      + '</head>\n'
+      + '<body>\n'
+      + '<div id="app" class="container-fluid">\n'
+      + '    <%- content %>\n'
+      + '</div>\n'
+      + '</body>\n'
+      + '<script src="<%- asset.vue %>"></script>\n'
+      + '<script src="<%- asset.vueStrap %>"></script>\n'
+      + '<script src="<%- asset.setup %>"></script>\n'
+      + '</html>\n',
 
-    // markbind assets
-    expect(mockFileSystem.containsDirectory(getPath('_site', 'markbind'))).toBeTruthy();
+    './site.json': '{\n'
+    + '  "baseUrl": "",\n'
+    + '  "pages": [\n'
+    + '    {\n'
+    + '      "src": "index.md",\n'
+    + '      "title": "Hello World"\n'
+    + '    }\n'
+    + '  ],\n'
+    + '  "ignore": [\n'
+    + '    "_site/*",\n'
+    + '    "*.json",\n'
+    + '    "*.md"\n'
+    + '  ],\n'
+    + '  "deploy": {\n'
+    + '    "message": "Site Update."\n'
+    + '  }\n'
+    + '}\n',
+  };
+  fs.vol.fromJSON(json, './');
+  console.log(fs.vol.toJSON());
+  console.log(fs.existsSync('/NUS Year 2/SEM2/markbind/markbind-cli/lib/template/page.ejs'));
 
-    // js
-    expect(mockFileSystem.containsDirectory(getPath('_site', 'markbind', 'js'))).toBeTruthy();
-    expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'js', 'setup.js'))).toBeTruthy();
-    expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'js', 'vue.min.js'))).toBeTruthy();
-    expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'js', 'vue-strap.min.js'))).toBeTruthy();
+  const site = new Site('./', '_site');
+  return site.generate().then(() => console.log(fs.vol.toJSON()));
 
-    // css
-    expect(mockFileSystem.containsDirectory(getPath('_site', 'markbind', 'css'))).toBeTruthy();
-    expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'css', 'bootstrap.min.css'))).toBeTruthy();
-    expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'css', 'github.min.css'))).toBeTruthy();
-    expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'css', 'markbind.css'))).toBeTruthy();
-
-    // fonts
-    expect(mockFileSystem.containsDirectory(getPath('_site', 'markbind', 'fonts'))).toBeTruthy();
-
-    console.log(mockFileSystem.mainDirectory
-      .subDirectories['D:']
-      .subDirectories['NUS Year 2']
-      .subDirectories.SEM2
-      .subDirectories.markbind
-      .subDirectories['markbind-cli']
-      .subDirectories['_site']
-      .subDirectories['markbind']
-      .subDirectories['js']);
-  });
+  // fs.mockFileSystem = mockFileSystem;
+  // walkSync.mockFileSystem = mockFileSystem;
+  // const site = new Site(path.resolve(''), path.resolve('_site'));
+  // return site.generate().then(() => {
+  //   // _site folder
+  //   expect(mockFileSystem.containsDirectory(getPath('_site'))).toBeTruthy();
+  //
+  //   // markbind assets
+  //   expect(mockFileSystem.containsDirectory(getPath('_site', 'markbind'))).toBeTruthy();
+  //
+  //   // js
+  //   expect(mockFileSystem.containsDirectory(getPath('_site', 'markbind', 'js'))).toBeTruthy();
+  //   expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'js', 'setup.js'))).toBeTruthy();
+  //   expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'js', 'vue.min.js'))).toBeTruthy();
+  //   expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'js', 'vue-strap.min.js'))).toBeTruthy();
+  //
+  //   // css
+  //   expect(mockFileSystem.containsDirectory(getPath('_site', 'markbind', 'css'))).toBeTruthy();
+  //   expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'css', 'bootstrap.min.css'))).toBeTruthy();
+  //   expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'css', 'github.min.css'))).toBeTruthy();
+  //   expect(mockFileSystem.containsFile(getPath('_site', 'markbind', 'css', 'markbind.css'))).toBeTruthy();
+  //
+  //   // fonts
+  //   expect(mockFileSystem.containsDirectory(getPath('_site', 'markbind', 'fonts'))).toBeTruthy();
+  //
+  //   console.log(mockFileSystem.mainDirectory
+  //     .subDirectories['D:']
+  //     .subDirectories['NUS Year 2']
+  //     .subDirectories.SEM2
+  //     .subDirectories.markbind
+  //     .subDirectories['markbind-cli']
+  //     .subDirectories['_site']
+  //     .subDirectories['markbind']
+  //     .subDirectories['js']);
+  // });
 });
